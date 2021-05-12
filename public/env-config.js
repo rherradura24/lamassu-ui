5a67f630b79e993a53036fe748a61a6f72aa3ddb
Saiz Haritz <hsaiz@ikerlan.es>
2021-05-12 14:37:53 +0200

    Front with env config

---
+++ public/env-config.js
@@ -0,0 +1,6 @@
+window._env_ = {
+    REACT_APP_AUTH_ENDPOINT: "http://localhost:8080/auth",
+    REACT_APP_AUTH_REALM: "SMARTCON-ONA",
+    REACT_APP_AUTH_CLIENT_ID: "frontend",
+    REACT_APP_CA_API: "http://dlt0.konnekt.ikerlan.es:5000/",        
+}
--- src/keycloak.js
+++ src/keycloak.js
@@ -3,9 +3,9 @@ import Keycloak from 'keycloak-js'
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloakProps = {
-    url: 'http://localhost:8080/auth',
-    realm: 'lamassuiot',
-    clientId: 'frontend',
+    url: window._env_.REACT_APP_AUTH_ENDPOINT,
+    realm: window._env_.REACT_APP_AUTH_REALM, 
+    clientId: window._env_.REACT_APP_AUTH_CLIENT_ID,
 }
 
 const keycloak = new Keycloak(keycloakProps)
