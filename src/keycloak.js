8d4c36b8552ae832462b100f9cacefe48c589b89
Haritz <hsaizsierra@gmail.com>
2022-03-10 22:31:06 +0100

    .env to env-config.js; fixed env variables

--- src/keycloak.js
+++ src/keycloak.js
@@ -3,9 +3,9 @@ import Keycloak from 'keycloak-js'
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloakProps = {
-    url: process.env.REACT_APP_AUTH_ENDPOINT,
-    realm: process.env.REACT_APP_AUTH_REALM, 
-    clientId: process.env.REACT_APP_AUTH_CLIENT_ID,
+    url: window._env_.REACT_APP_AUTH_ENDPOINT,
+    realm: window._env_.REACT_APP_AUTH_REALM, 
+    clientId: window._env_.REACT_APP_AUTH_CLIENT_ID,
 }
 
 const keycloak = new Keycloak(keycloakProps)
