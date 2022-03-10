4c5efc347ea421150386a92fe49158852763be49
Haritz <hsaizsierra@gmail.com>
2022-03-10 22:09:31 +0100

    Added bash entrypoint

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+console.log(process.env)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
