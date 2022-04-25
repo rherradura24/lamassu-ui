bd2a995bff61d62f6cc68ea65da572f9d2fbcb63
Haritz <hsaizsierra@gmail.com>
2022-04-25 10:54:29 +0200

    fixed docker image urls

--- env-docker-config.js
+++ env-docker-config.js
@@ -3,8 +3,8 @@ window._env_ =  {
     REACT_APP_AUTH_REALM: "lamassu",
     REACT_APP_AUTH_CLIENT_ID: "frontend",
 
-    REACT_APP_LAMASSU_CLOUD_PROXY_API: "https://dev-lamassu.zpd.ikerlan.es:8081",
-    REACT_APP_LAMASSU_CA_API: "https://dev-lamassu.zpd.ikerlan.es/api/ca",
-    REACT_APP_LAMASSU_DMS_ENROLLER_API: "https://dev-lamassu.zpd.ikerlan.es/api/dmsenroller",
-    REACT_APP_LAMASSU_DEVMANAGER: "https://dev-lamassu.zpd.ikerlan.es/api/devmanager"
+    REACT_APP_LAMASSU_CLOUD_PROXY_API: "https:/${DOMAIN}:8081",
+    REACT_APP_LAMASSU_CA_API: "https:/${DOMAIN}/api/ca",
+    REACT_APP_LAMASSU_DMS_ENROLLER_API: "https:/${DOMAIN}/api/dmsenroller",
+    REACT_APP_LAMASSU_DEVMANAGER: "https:/${DOMAIN}/api/devmanager"
 }
