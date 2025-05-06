ebddc356a444aa8ffed2e6271b65de4619c7470e
Haritz S. Sierra <hsaizsierra@gmail.com>
2025-05-06 08:26:23 +0200

    fix: typo in sidebar Notifications element

--- src/App.tsx
+++ src/App.tsx
@@ -125,7 +125,7 @@ export default function Dashboard () {
             ]
         },
         {
-            sectionTitle: "Notificaitons",
+            sectionTitle: "Notifications",
             sectionItems: [
                 {
                     kind: "navigation",
